928558b7f52bfbb590e44a1f4032c7d15fe8ebc2
ANAND KUMAR JHA <47267363+anandjha1@users.noreply.github.com>
2023-04-18 11:31:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const itemInputEl = document.getElementById('itemInput')
 const btnSubmitEl = document.getElementById('btnSubmit')
 const listItemsEl = document.getElementById('listItems')
 
-updateList()
+renderList()
 
 // add Event listener
 btnSubmitEl.addEventListener('click', addItemToArray)
@@ -16,24 +16,33 @@ btnSubmitEl.addEventListener('click', addItemToArray)
 // functions
 function addItemToArray() {
     const value = itemInputEl.value
+    resetField()
+
     list.push(value)
-    updateList()
+    renderList()
 }
 
 
-function updateList(){
+function renderList(){
     // reset list
     listItemsEl.innerHTML = ''
 
-    list.forEach(item=>{
+    list.forEach((item, i)=>{
         const li = document.createElement('li')
         li.innerHTML = item
         li.addEventListener('dblclick', removeItem)
+        li.setAttribute('value', i)
         // li.setAttribute('id', item)
         listItemsEl.append(li)
     })
 }
 
-function removeItem(){
-    console.log('...remove called')
+function removeItem(event){
+    const item = event.target
+    list.splice(item.value, 1)
+    renderList()
+}
+
+function resetField(){
+    itemInputEl.value = ''
 }
